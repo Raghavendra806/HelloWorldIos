4a53c113a8393fbfd7ceda5040e7a3dcfffd940e
Raghavendra Naik <ragnai@nasdaq.com>
2021-05-05 13:05:22 +0530

    Added project

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Ravi Raja Jangid on 05/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Ravi Raja Jangid on 05/05/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
